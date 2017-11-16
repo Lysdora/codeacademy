5ebe117390920531c35d0a4b70291c1ed6426a86
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-16 17:24:07 +0100

    up

--- helpers/array.js
+++ helpers/array.js
@@ -20,3 +20,16 @@ console.log(druide[4]);
 console.log(druide[4][1]);
 
 
+// Changer la valeur d'un objet
+mage.nom = "Mage de glace";
+console.log(mage);
+
+// changer la valeur d'une propriete d'un tableau
+druide[2] = "puissance";
+console.log(druide);
+
+var stats = ["attaque", "defense"];
+console.log(stats);// avant le changement
+stats[1] = "puissance";
+console.log(stats);// modification de defense en puissance
+
