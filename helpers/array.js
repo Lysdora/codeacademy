4942df51157b773992d95b22e6fac56578d507dc
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-16 17:17:14 +0100

    add

---
+++ helpers/array.js
@@ -0,0 +1,22 @@
+// Table
+var alliance = ["elfe", "humain", "nain", "draeni", "gnome"];
+
+var druide = ["nature", "vert", "mana", "agilité", alliance];
+console.log(druide);
+
+// objet
+var mage = {
+    nom : "Mage de Feu",
+    talent: "feu"
+
+};
+
+console.log(druide[0]); // nature
+
+// acceder à un tableau qui est dans un tableau
+console.log(druide[4]);
+
+// acceder à humain
+console.log(druide[4][1]);
+
+
---
+++ helpers/length.js
@@ -0,0 +1,6 @@
+var nom = "Toto";
+// Nombre de lettre dans la var toto = 4
+console.log(nom.length);
+
+console.log(nom[0]);// affiche la lettre T
+console.log(nom[nom.length - 1]); // acceder a la derniere lettre
